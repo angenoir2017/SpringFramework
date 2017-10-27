de74f8933dacbe34be654aeae6122a6fdffac544
langenoir <angenoir2017@gmail.com>
2017-10-27 14:02:20 +0000

    Mise a jour des dependences spring security data test boot

---
+++ src/main/java/com/rockteam/releves/hello.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.rockteam.releves;
+
+/**
+ *
+ * @author Langenoir
+ */
+public class hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("salut la famille");  
+    }
+    
+}
